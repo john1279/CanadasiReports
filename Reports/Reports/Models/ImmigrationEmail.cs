d60b460ca3767514c7ad03e9f18de1db2e7df638
John <ALb3rt1602@gmail.com>
2019-12-06 09:24:47 -0500

    creada las clases de las tablas

---
+++ Reports/Reports/Models/ImmigrationEmail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reports.Models
+{
+    [Table("email_addr_bean_rel")]
+    public class ImmigrationEmail
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.None)]
+
+        [Column("email_address")]
+        public string email_address { get; set; }
+
+
+    }
+}
