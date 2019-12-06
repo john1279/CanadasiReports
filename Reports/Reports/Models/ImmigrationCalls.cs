1dbe7851bb4427f3b85f0def3f581e0faed6563d
John <ALb3rt1602@gmail.com>
2019-12-06 10:04:29 -0500

    modificaciones

---
+++ Reports/Reports/Models/ImmigrationCalls.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reports.Models
+{
+    [Table("calls")]
+    public class ImmigrationCalls
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.None)]
+
+        [Column("id")]
+        public string id { get; set; }
+
+        [Column("date_start")]
+        public string date_start { get; set; }
+
+                
+}
+}
---
+++ Reports/Reports/Models/ImmigrationUsers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reports.Models
+{
+    [Table("users")]
+   
+    public class ImmigrationUsers
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.None)]
+
+        [Column("user_name")]
+        public string user_name { get; set; }
+
+
+    }
+}
